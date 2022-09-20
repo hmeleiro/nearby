958a264ca6eef84904c4c53ad0cb210767a5311d
hmeleiros <hmeleiros@gmail.com>
2022-09-20 15:27:54 +0200

    First commit

---
+++ app.R
@@ -0,0 +1,99 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(mapdeck)
+library(osmdata)
+library(sf)
+library(dplyr)
+
+mb_token = 'pk.eyJ1IjoiaG1lbGVpcm9zIiwiYSI6ImNqcGk0OG1nazEwMzMzcXBmbnAydzUxYnEifQ.mznrc0j3W0nyqnTw8p6JqA'
+set_token(mb_token)
+
+
+getOsmData <- function(circle_sf) {
+  q <- opq(bbox = st_bbox(circle_sf)) %>%
+    add_osm_feature("amenity", "cinema")
+  
+  data <- osmdata_sf(q)
+  return(data) 
+}
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  tags$script(src = "getGeoLoc.js"),
+  
+  h1("NearBy"),
+  
+  mapdeckOutput(outputId = "map", height = "800px")
+  
+  
+  
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+  
+  
+  data_sf <- reactive({
+    point_sf <- tibble(lat = input$lat, long = input$long)
+    point_sf <- st_as_sf(point_sf, coords = c("long", "lat"), crs = 4326)
+    circle_sf <- st_buffer(point_sf, dist = 10000)
+    
+    list("point_sf" = point_sf, "circle_sf" = circle_sf)
+  })
+  
+  dataOSM <- reactive({
+    getOsmData(data_sf()$circle_sf)
+  })
+  
+  output$map <- renderMapdeck({
+    # Note that our mb_token value is read in from global.R
+    mapdeck(
+      style = mapdeck_style("light"), 
+      # 55 degrees off of north.
+      bearing = -55, 
+      location = c(input$long, input$lat),
+      zoom = 12
+    )
+  })
+  
+  observeEvent({input$geolocation}, {
+    req(input$geolocation)
+    print(input$geolocation)
+    print(nrow(dataOSM()$osm_polygons))
+    
+    # MAPBOX
+    mapdeck_update(map_id = 'map', session = session) %>%
+      # add_sf(
+      #   data = data_sf()$circle_sf,
+      #   fill_opacity = 0.3,
+      #   stroke_opacity = 1,
+      #   focus_layer = F, 
+      #   update_view = F
+      # ) %>%
+      add_sf(
+        data = data_sf()$point_sf, 
+        radius_min_pixels = 6,
+        update_view = F
+      ) %>%
+      add_sf(
+        data = dataOSM()$osm_polygons, 
+        fill_opacity = 1,
+        fill_colour = "#202020", 
+        update_view = F
+      )   
+    
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
