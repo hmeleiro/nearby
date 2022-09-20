e3baf37677cd21b0a6a294d8af20c9cc3b8ff8e5
hmeleiro <hmeleiros@gmail.com>
2022-09-20 20:31:23 +0200

    Style advances

--- app.R
+++ app.R
@@ -8,20 +8,56 @@ mb_token = 'pk.eyJ1IjoiaG1lbGVpcm9zIiwiYSI6ImNqcGk0OG1nazEwMzMzcXBmbnAydzUxYnEif
 set_token(mb_token)
 
 
-getOsmData <- function(circle_sf) {
-  q <- opq(bbox = st_bbox(circle_sf)) %>%
-    add_osm_feature("amenity", "cinema")
+getOsmData <- function(circle_sf, feature, tag) {
   
-  data <- osmdata_sf(q) 
+  tryCatch({
+    q <- opq(bbox = st_bbox(circle_sf)) %>%
+      add_osm_feature(feature, tag)
+    
+    message("Searching in OSM...")
+    data <- osmdata_sf(q) 
+    message("Got the results...")
+  }, error = function(e) {
+    data <- NULL
+  })
+  
   return(data) 
 }
+
+features <- readRDS("data/OSMfeatures.rds")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
   tags$script(src = "getGeoLoc.js"),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  ),
   
   h1("NearBy"),
+  
+  shiny::inputPanel(
+    div(
+      selectInput(
+        inputId = "feature", 
+        label = "Choose feature",
+        choices = unique(features$features), 
+        multiple = T, selectize = T),
+      
+      selectInput(
+        inputId = "tag", 
+        label = "Choose tag",choices = NULL,
+        multiple = T, selectize = T),
+      
+      sliderInput(inputId = "dist", label = "At a distance of...",
+                  min = 1000, max = 100000, 
+                  animate = T, sep = ",", 
+                  post = "m", value = 2000),
+      actionButton(inputId = "SearchBtn", label = "Search"),
+      class = "input-entries"
+    ),
+
+  ),
   
   mapdeckOutput(outputId = "map", height = "800px")
   
@@ -32,43 +68,84 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
+  startFeature <- reactive({
+    sample(unique(features$features), 1)
+  })
+  
+  observe(
+    updateSelectizeInput(
+      session = session,
+      inputId = "feature",
+      selected = startFeature(),
+      # selected = NULL,
+      options = list(
+        create = F,
+        maxItems = 1,
+        placeholder = 'Search an OSM feature',
+        hideSelected = T
+      )
+    )
+  )
+  
+  observe({
+    req(input$feature)
+    updateSelectizeInput(
+      session = session,
+      inputId = "tag",
+      choices = features$tags[features$features == input$feature],
+      options = list(
+        create = F,
+        maxItems = 3,
+        placeholder = 'Type an OSM tag',
+        hideSelected = T,
+        closeAfterSelect = T
+        
+      )
+    )
+  }
+  
+  )
   
   data_sf <- reactive({
+    req(input$geolocation)
     point_sf <- tibble(lat = input$lat, long = input$long)
     point_sf <- st_as_sf(point_sf, coords = c("long", "lat"), crs = 4326)
-    circle_sf <- st_buffer(point_sf, dist = 10000)
+    circle_sf <- st_buffer(point_sf, dist = input$dist)
     
     list("point_sf" = point_sf, "circle_sf" = circle_sf)
   })
   
   dataOSM <- reactive({
-    getOsmData(data_sf()$circle_sf)
+    req(input$tag)
+    getOsmData(data_sf()$circle_sf, input$feature, input$tag)
   })
   
   output$map <- renderMapdeck({
+    userLoc <- data_sf()$point_sf
+    
     # Note that our mb_token value is read in from global.R
     mapdeck(
       style = mapdeck_style("light"), 
-      # 55 degrees off of north.
-      bearing = -55, 
+      # bearing = -55, 
       location = c(input$long, input$lat),
-      zoom = 12
-    )
+      zoom = 16
+    ) %>%
+      add_sf(
+        data = userLoc , 
+        radius_min_pixels = 6,
+        update_view = F
+      )
   })
   
-  observeEvent({input$geolocation}, {
-    req(input$geolocation)
-    print(input$geolocation)
-    print(nrow(dataOSM()$osm_polygons))
+  observeEvent({input$tag}, {
+    req(input$geolocation & nrow(dataOSM()$osm_polygons) > 0)
     
-    
-    userLoc <- data_sf()$point_sf
+    print( nrow(dataOSM()$osm_polygons) )
     polygons <- dataOSM()$osm_polygons %>% 
       mutate(
         name = iconv(name, from="UTF-8", to="UTF-8"),
         popUp = sprintf(
-          "<p class=popup-text>%s</p>
-          <p class=popup-text>Type: %s</p>", 
+          "<p class=popup-text>%s (%s)</p>", 
           name, amenity
         )
       )
@@ -82,18 +159,13 @@ server <- function(input, output, session) {
       #   stroke_width = 3,
       #   focus_layer = F,
       #   update_view = F
-      # ) %>%
-      add_sf(
-        data =userLoc , 
-        radius_min_pixels = 6,
-        update_view = F
-      ) %>%
+      # )  %>%
       add_sf(
         data = polygons, 
         fill_opacity = 1,
         fill_colour = "#202020",
         # fill_colour = "color",
-        update_view = F,
+        update_view = T,
         tooltip = "popUp"
       )   
     
--- prueba.R
+++ prueba.R
@@ -7,8 +7,35 @@ point_sf <- st_as_sf(point_sf, coords = c("long", "lat"), crs = 4326)
 circle_sf <- st_buffer(point_sf, dist = 10000)
 
 
-data <- getOsmData(circle_sf)
+data <- getOsmData(circle_sf, tag = c("bank", "arts_centre", "atm"))
 
 
 x <- data$osm_polygons 
 
+
+features <- available_features()
+x <- NULL
+for (i in 1:length(features)) {
+  message(features[i])
+  tmp <- available_tags(features[i])
+  
+  if(length(tmp) > 0) {
+    tmp <- tibble(features = features[i], tags = tmp)
+    x <- rbind(x, tmp)
+  }
+
+}
+
+saveRDS(x, "data/OSMfeatures.rds")
+
+
+tags <- available_tags("shop")
+library(stringr)
+names <- str_replace_all(tags, "_", " ") %>% str_to_title()
+names <- if_else(names %in% c("Bbq", "Atm"), toupper(names), names)
+names(tags) <- names
+
+saveRDS(tags, "data/amenity_tags.rds")
+
+writeLines(tags, "data/amenity_tags.txt")
+readLines("data/amenity_tags.txt")
